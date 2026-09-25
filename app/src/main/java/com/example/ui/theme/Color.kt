8753113a9f7fda17d7a91bd107fc168de08f748b
Mohammad Maruf <mahdihasanmaruf9076@gmail.com>
2026-09-25 19:26:16 +0600

    Update Color.kt

--- app/src/main/java/com/example/ui/theme/Color.kt
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -2,45 +2,52 @@ package com.example.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Primary purple
 val PurplePrimary = Color(0xFF8B5CF6)
 val PurpleLight = Color(0xFFA78BFA)
 val PurpleDark = Color(0xFF7C3AED)
 
-// App background
 val AppBackground = Color(0xFF151419)
 
-// Cards / surfaces
 val AppSurface = Color(0xFF211F26)
 val AppSurfaceVariant = Color(0xFF2A2730)
 val AppSurfaceElevated = Color(0xFF302C36)
 
-// Text
 val TextPrimary = Color(0xFFF5F3F7)
 val TextSecondary = Color(0xFFA8A5AE)
 val TextMuted = Color(0xFF77737D)
 
-// Navigation
 val NavigationBackground = Color(0xFF211F26)
 val NavigationSelected = Color(0xFF5A506F)
 val NavigationIcon = Color(0xFFF5EEFF)
 val NavigationUnselected = Color(0xFFA8A5AE)
 
-// Status colors
 val SuccessGreen = Color(0xFF39D353)
 val InfoBlue = Color(0xFF58A6FF)
 val WarningOrange = Color(0xFFF2CC60)
 val ErrorRed = Color(0xFFF85149)
 
-// GitHub-style colors
 val GitHubGreen = Color(0xFF39D353)
 val GitHubBlue = Color(0xFF58A6FF)
 val GitHubPurple = Color(0xFFA371F7)
 
-// Divider / border
 val AppDivider = Color(0xFF35323A)
 val AppBorder = Color(0xFF3A3640)
 
-// White / black
 val PureWhite = Color(0xFFFFFFFF)
 val PureBlack = Color(0xFF000000)
+
+/*
+ * Contribution heatmap colors
+ */
+
+val HeatmapLevel0Dark = Color(0xFF2A2730)
+val HeatmapLevel1Dark = Color(0xFF0E4429)
+val HeatmapLevel2Dark = Color(0xFF006D32)
+val HeatmapLevel3Dark = Color(0xFF26A641)
+val HeatmapLevel4Dark = Color(0xFF39D353)
+
+val HeatmapLevel0Light = Color(0xFFEBEDF0)
+val HeatmapLevel1Light = Color(0xFF9BE9A8)
+val HeatmapLevel2Light = Color(0xFF40C463)
+val HeatmapLevel3Light = Color(0xFF30A14E)
+val HeatmapLevel4Light = Color(0xFF216E39)
